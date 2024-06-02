263c057b8dcd681a2d201292e8a72049ad59003d
clindv <clindv@protonmail.com>
2024-06-03 05:31:27 +0800

    fix regex

--- src/tag/core.clj
+++ src/tag/core.clj
@@ -33,7 +33,7 @@
 (defn annotate
   [input-string threshold]
   (->> input-string
-       (#(clojure.string/split % #"[^\w'-]+|[_]+"))
+       (#(clojure.string/split % #"[^\w']+|[\d+_-]+"))
        (map clojure.string/lower-case)
        distinct
        (map #(list % (vocabulary %)))
